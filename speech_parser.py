fc3fdf061b81f1287334da3b96c3ce6f66c1914f
Jori Romans <jromans@ualberta.ca>
2020-01-29 23:36:05 -0700

    Added HTML to Text conversion, removing all unneccasary HTML data and converting <br> to new lines

--- speech_parser.py
+++ speech_parser.py
@@ -1,11 +1,15 @@
 import requests
+import re
 
 class SpeechParser:
 
     def __init__(self, url):
         self.source = None
+        self.text = None
+        self.info = {}
 
         self.setSourceHTML(url)
+        self.parseText()
 
 
     def setSourceHTML(self, url):
@@ -46,9 +50,55 @@ class SpeechParser:
 
         return self.source
 
-# s = SpeechParser("https://www.assembly.ab.ca/Documents/isysquery/0b24aeb4-4082-4331-8934-302c92e0950b/1/doc/")
-#
-# with open("test.txt", "w") as f:
-#     source = s.getSourceHTML()
-#     for l in source:
-#         f.write(l)
+    def getSourceText(self):
+        """Gets the source text (converted from HTML) stored in object and returns as a string. The object must have been initialized properly for this to return a valid value.
+
+        Args:
+            N/A
+        Returns:
+            String: Returns raw text string converted from HTML URL.
+            None: Returns none type upon failed retrieval of text.
+
+        """
+
+        return self.text
+
+    def HTMLToText(self):
+        """Converts source html to natural text, removing any unwanted components
+
+        Args:
+            N/A
+        Returns:
+            N/A
+        Notes:
+            May need to be changed if HTML structure changes across documents
+
+        """
+        text = self.source
+        text = text.replace('\n', '') # remove all new line as HTML new line does not represent text new line
+        text = text.replace('<br />', '\n') # replace all HTML breaks with new line
+        text = re.sub(r'<[ \v\t\S]*>', '', text) # remove all left over HTML context
+
+        self.text = text[text.find('\n') + 1:] # TODO: Implementation leaves one line of HTML context at the beginning. Could not figure out how to work around it
+
+
+    def parseText(self):
+        """Parses text stored in the HTML into a dict response structure to be passed to blob storage.
+
+        Args:
+            N/A
+        Returns:
+            N/A
+
+        """
+        self.HTMLToText()
+
+    def parseSpeeches(self):
+        pass
+
+    def getInfo(self):
+        return self.info
+
+s = SpeechParser('https://www.assembly.ab.ca/Documents/isysquery/0b24aeb4-4082-4331-8934-302c92e0950b/1/doc/')
+with open('test2.txt', 'w') as f:
+    f.write(s.getSourceText())
