fd43fb4cc97b99d1dc67eae9bd9fd76189262a94
Zach Drever <zdrever@ualberta.ca>
2020-02-05 22:43:09 -0700

    [feat] include new data-preprocessing scripts (#1)
    Begin organizing code into modules

---
+++ etl/preprocess/webscraping/get_docs.py
@@ -0,0 +1,17 @@
+from sys import argv
+from os.path import abspath
+from utils import get_urls, overwrite_urls
+
+
+HANSARD_SESSION_URL ='https://www.assembly.ab.ca/net/index.aspx?p=han&section=doc&fid=1'
+
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        raise Exception("Usage: python3 get_urls.py <out_file_path> [<hansard_url>]")
+    elif len(argv) == 3:
+        HANSARD_SESSION_URL = argv[2]
+
+    filepath = abspath(argv[1])
+    df = get_urls(HANSARD_SESSION_URL)
+    overwrite_urls(df, filepath)
