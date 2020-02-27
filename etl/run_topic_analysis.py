0d28950307d0b2050775dff8839025991fae90ac
zdrever <zdrever@ualberta.ca>
2020-02-26 18:29:02 -0700

    [feat] topic analysis complete
    Using LDA from sklearn in order to generate topics for each MLA. Currently topics are based on the top 1000 sentence summaries from each MLA. Number of components (topics) is set to 15, and the number of words per topic is also set to 15. These values can be changed by changing parameters at the top of the TopicAnalyzer class.

---
+++ etl/run_topic_analysis.py
@@ -0,0 +1,50 @@
+from pkgutil import get_data
+from pandas import DataFrame
+from storage_clients import MySqlClient
+from topic_analysis import TopicAnalyzer
+from nltk.stem.snowball import EnglishStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+
+top_n_sentences = 1000
+stopwords = {word.strip() for word in open('data/topic_stopwords.txt', 'r')}
+
+"""
+Create dataframe of top topics per MLA
+and upload to the database
+"""
+
+
+def generate_topics_per_mla():
+    topics = []
+    mysql_client = MySqlClient()
+    mlas = mysql_client.read_data('SELECT * FROM mlas')
+
+    for last_name, mla_id in zip(mlas['MLALastName'], mlas['RidingNumber']):
+        print(f'creating topics for {last_name}')
+        try: 
+            data = mysql_client.read_data(
+            f'SELECT * FROM {last_name} ORDER BY Rank LIMIT {top_n_sentences}')
+        except: 
+            print(f'Could not execute query for {last_name}')
+            continue
+        corpus = list(data['Sentence'])
+        ta = TopicAnalyzer(corpus, stopwords)
+        try: 
+            mla_topics = ta.analyze()
+        except Exception as e:
+            print(f'Could not run analysis for {last_name}')
+            print(e)
+
+        for topic_rank, topic_words in enumerate(mla_topics):
+            topics.append({
+                'MLAId': mla_id,
+                'TopicRank': topic_rank,
+                'TopicWords': topic_words
+            })
+    topics_table = DataFrame(topics)
+    mysql_client.write_data('topics', topics_table)
+
+
+if __name__ == '__main__':
+    generate_topics_per_mla()
