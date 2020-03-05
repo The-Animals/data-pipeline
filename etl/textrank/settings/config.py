c41b7069abe208be144cb04d42c3c6344b581b39
zdrever <zdrever@ualberta.ca>
2020-03-05 15:13:56 -0700

    [chore] remove old text rank null words

--- etl/textrank/settings/config.py
+++ etl/textrank/settings/config.py
@@ -11,16 +11,3 @@ epsilon = 1e-4 # epsilon value for algorithm
 damping = 0.85 # damping value for algorithm
 delta = 1e-7 # delta value for algorithm
 # ---------------------------------------------------------------------------------------------
-
-
-# Code
-# ---------------------------------------------------------------------------------------------
-
-# Create stopwords
-# with open('{0}\\stopwords\\{1}'.format(os.path.dirname(os.path.realpath(__file__)), stopwordsFile), 'r') as f:
-#     words = ()
-#     for word in f.readlines():
-#         words += (word.rstrip(),)
-#     stopwords = frozenset(words)
-
-# ---------------------------------------------------------------------------------------------
